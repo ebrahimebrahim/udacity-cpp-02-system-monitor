17abdb266db2ef9c866a005d098f4d4634ad1ccf
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2020-11-01 23:55:54 -0500

    add failing tests and first attempt at split

--- include/util.h
+++ include/util.h
@@ -2,13 +2,32 @@
 #include <vector>
 
 /**
- *  @brief  Split a string based on a token
+ *  @brief  Split a string based on a separator
  *  @param str  The string to split
- *  @param substr  The token to split by
+ *  @param separator  The separator to split by
  *  @return  The split as a vector of strings.
  */
-std::vector<std::string> split(const std::string& str,
-                               const std::string& substr = " ") {
+std::vector<std::string> split(const std::string& str, const std::string& separator) {
+  std::vector<std::string> out;
+  
+  int token_start = 0;
+
+  size_t separator_start;
+  while ((separator_start = str.find(separator,token_start)) != std::string::npos){
+    int token_length = separator_start - token_start;
+    out.push_back(str.substr(token_start,token_length));
+    token_start += token_length + separator.length();
+  }
+
+  return out;
+}
+
+/**
+ *  @brief  Split a string by whitespace
+ *  @param str  The string to split
+ *  @return  The split as a vector of strings.
+ */
+std::vector<std::string> split_whitespace(const std::string& str) {
   std::vector<std::string> out;
   return out;
 }
--- test/test.cpp
+++ test/test.cpp
@@ -5,6 +5,66 @@
 
 using vecstr = std::vector<std::string>;
 
-TEST_CASE( "default split two words", "[split]" ) {
-    REQUIRE( split("hello world") == vecstr({"hello", "world"}) );
+TEST_CASE( "split one word", "[split]" ) {
+    REQUIRE( split("hello"," ") == vecstr{"hello"} );
+}
+
+TEST_CASE( "split basic", "[split]" ) {
+    REQUIRE( split("hello world"," ") == vecstr{"hello", "world"} );
+}
+
+TEST_CASE( "split initial tokens", "[split]" ) {
+    REQUIRE( split("  hello world"," ") == vecstr{"", "", "hello", "world"} );
+}
+
+TEST_CASE( "split final tokens", "[split]" ) {
+    REQUIRE( split("hello world  "," ") == vecstr{"hello", "world", "", ""} );
+}
+
+TEST_CASE( "split empty string", "[split]" ) {
+    REQUIRE( split("", " ") == vecstr{""} );
+}
+
+TEST_CASE( "split string with only token", "[split]" ) {
+    REQUIRE( split("t","t") == vecstr{"",""} );
+}
+
+TEST_CASE( "split string with only long token", "[split]" ) {
+    REQUIRE( split("token","token") == vecstr{"",""} );
+}
+
+TEST_CASE( "split string with long token", "[split]" ) {
+    REQUIRE( split("hellotokenworld","token") == vecstr{"hello","world"} );
+}
+
+TEST_CASE( "split many words with long token", "[split]" ) {
+    REQUIRE( split("hellotokenworldtokenit'stokenatokengreattokenday","token") == vecstr{"hello","world","it's","a","great","day"} );
+}
+
+TEST_CASE( "split many words", "[split]" ) {
+    REQUIRE( split("hello world it's just another day"," ") == vecstr{"hello", "world", "it's", "just", "another", "day"} );
+}
+
+TEST_CASE( "split_whitespace empty string", "[split]" ) {
+    REQUIRE( split_whitespace("").empty() );
+}
+
+TEST_CASE( "split_whitespace only whitespace", "[split]" ) {
+    REQUIRE( split_whitespace("  \n   \t \n \t  ").empty() );
+}
+
+TEST_CASE( "split_whitespace many words", "[split]" ) {
+    REQUIRE( split_whitespace("hello world it's just another day") == vecstr{"hello", "world", "it's", "just", "another", "day"} );
+}
+
+TEST_CASE( "split_whitespace many words lots of space", "[split]" ) {
+    REQUIRE( split_whitespace("hello     world it's \njust\tanother  \n day") == vecstr{"hello", "world", "it's", "just", "another", "day"} );
+}
+
+TEST_CASE( "split_whitespace initial whitespace", "[split]" ) {
+    REQUIRE( split_whitespace("   hello  world") == vecstr{"hello", "world"} );
+}
+
+TEST_CASE( "split_whitespace final whitespace", "[split]" ) {
+    REQUIRE( split_whitespace("hello  world      ") == vecstr{"hello", "world"} );
 }
